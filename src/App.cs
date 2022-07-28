d0b4a5221c4cb8a40ef65feecf40a88cb029bca4
typeperfest <perfect2111@gmail.com>
2022-07-28 13:14:38 +0300

    raw concept

---
+++ src/App.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+public class App 
+{
+    private readonly string Token;
+
+    App(string token) 
+    {
+        Token = token;
+    }
+    public void Connect() 
+    {
+        
+    }
+
+    public void BeginTrade() 
+    {
+
+    }
+
+    public void StopTrade() 
+    {
+
+    }
+}
